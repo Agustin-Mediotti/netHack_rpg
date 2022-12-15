d026e01d7b823d4a414505f4eeb6ad9fca251617
net_creature <agustinmediotti@gmail.com>
2022-12-15 18:54:05 -0300

    added Rect struct, fn create_room, fn create_h_tunnel, fn create_v_tunnel

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
     File Author: NetCreature
 */
 
+use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -95,15 +96,64 @@ struct Game {
 /* Fill the map with "unblocked" tiles */
 
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
     // Here we use the macro vec! to create a Vec and fill it with values
 
-    // placing testing tiles
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
+
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    } // Use min and max to make sure the .. range always start with the of the numbers
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+/*
+    Warn: We are using the Tile::empty to "carve out" the empty tiles in the map. That means we're replacing the existing
+    tiles with a new, empty one instead of just modifying its blocked and block_sight fields. It wouldn't work if the tiles
+    already had some fields you care about.
+
+    ToDo: Refactor the tunneling carving to check and modify the properties of the selected Tile.
+*/
 
 fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     use tcod::input::Key;
@@ -180,7 +230,7 @@ fn main() {
 
     /* Declared game objects */
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let player = Object::new(25, 23, '@', WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
     let mut objects = [player, npc];
