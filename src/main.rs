21b4721e6e65f26acda5e6fa39c3b400f901f016
net_creature <agustinmediotti@gmail.com>
2022-12-14 23:59:41 -0300

    added dependencies, font & notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+/*
+    netHack_rpg by NetCreature
+
+    compile & run: cargo run --release
+
+    some notes:
+    '--release' tag compiles the code with optimizations making it faster but worst on debugger.
+    'tcod' is the bindings for 'libtcod'.
+
+
+*/
+
+use tcod::colors::*;
+use tcod::console::*;
+
 fn main() {
     println!("Hello, world!");
 }
