f9d29552c6c875525887d5841c070302c1d967e8
net_creature <agustinmediotti@gmail.com>
2022-12-22 12:12:25 -0300

    added procedural dungeon

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
     File Author: NetCreature
 */
 
+use rand::Rng;
 use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
@@ -14,6 +15,10 @@ const LIMIT_FPS: i32 = 20;
 
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color {
@@ -95,20 +100,61 @@ struct Game {
 
 /* Fill the map with "unblocked" tiles */
 
-fn make_map() -> Map {
+fn make_map(player: &mut Object) -> Map {
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
     // Here we use the macro vec! to create a Vec and fill it with values
 
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
+    let mut rooms = vec![];
 
-    create_h_tunnel(25, 55, 23, &mut map);
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE..ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE..ROOM_MAX_SIZE + 1);
+        let x = rand::thread_rng().gen_range(0..MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0..MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        /* Run through the other rooms and see if they intersect with this one */
+
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            // This means no interaction, valid room
+            create_room(new_room, &mut map); // Paint it to the map's tiles
+
+            let (new_x, new_y) = new_room.center(); // Center coordinates of the room
+
+            if rooms.is_empty() {
+                // This is the first room, where the player starts at
+                player.x = new_x;
+                player.y = new_y;
+            } else {
+                /* All rooms after the first, connect it to the previous room with a tunnel */
+
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center(); // Center coordinates with previous room
+
+                /*  Random bool value */
+                if rand::random() {
+                    // First horizontally
+                    create_v_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_h_tunnel(prev_y, new_y, new_x, &mut map)
+                } else {
+                    // First Vertically
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_v_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            rooms.push(new_room);
+        }
+    }
 
     map
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Rect {
     x1: i32,
     y1: i32,
@@ -125,6 +171,19 @@ impl Rect {
             y2: y + h,
         }
     }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    } // Returns true if this rectangle intersects with another one
 }
 
 fn create_room(room: Rect, map: &mut Map) {
@@ -230,12 +289,14 @@ fn main() {
 
     /* Declared game objects */
 
-    let player = Object::new(25, 23, '@', WHITE);
+    let player = Object::new(0, 0, '@', WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
     let mut objects = [player, npc];
 
-    let game = Game { map: make_map() };
+    let game = Game {
+        map: make_map(&mut objects[0]),
+    };
 
     /* Main loop */
 
